ddb1ee3916a13a55ae115321dc9704f8a5ceafe2
KyrianU <kyrian.uwaegbulem@gmail.com>
2023-01-11 17:43:36 +0000

    Added toasts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -80,6 +80,8 @@ TEMPLATES = [
         },
     },
 ]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
